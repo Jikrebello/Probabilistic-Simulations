2617aea121730a027ddc068045ecb3318d2d7c16
Jikrebello <33652252+Jikrebello@users.noreply.github.com>
2021-05-19 23:39:10 +0200

    Add files via upload

---
+++ Probabilistic Simulations/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace Probabilistic_Simulations
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtDisplay = new System.Windows.Forms.TextBox();
+            this.BtnPlinko = new System.Windows.Forms.Button();
+            this.BtnShootingGallery = new System.Windows.Forms.Button();
+            this.numGames = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numPeopleShooting = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numDrivingTests = new System.Windows.Forms.NumericUpDown();
+            this.BtnDrivingSchool = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numWheelSpins = new System.Windows.Forms.NumericUpDown();
+            this.BtnRouletteWheel = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numTimesPaidForToy = new System.Windows.Forms.NumericUpDown();
+            this.BtnToyCollectables = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numGames)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPeopleShooting)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDrivingTests)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numWheelSpins)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTimesPaidForToy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TxtDisplay
+            // 
+            this.TxtDisplay.Location = new System.Drawing.Point(12, 12);
+            this.TxtDisplay.Multiline = true;
+            this.TxtDisplay.Name = "TxtDisplay";
+            this.TxtDisplay.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtDisplay.Size = new System.Drawing.Size(448, 139);
+            this.TxtDisplay.TabIndex = 0;
+            // 
+            // BtnPlinko
+            // 
+            this.BtnPlinko.Location = new System.Drawing.Point(12, 157);
+            this.BtnPlinko.Name = "BtnPlinko";
+            this.BtnPlinko.Size = new System.Drawing.Size(75, 51);
+            this.BtnPlinko.TabIndex = 1;
+            this.BtnPlinko.Text = "Plinko";
+            this.BtnPlinko.UseVisualStyleBackColor = true;
+            this.BtnPlinko.Click += new System.EventHandler(this.BtnPlinko_Click);
+            // 
+            // BtnShootingGallery
+            // 
+            this.BtnShootingGallery.Location = new System.Drawing.Point(12, 273);
+            this.BtnShootingGallery.Name = "BtnShootingGallery";
+            this.BtnShootingGallery.Size = new System.Drawing.Size(75, 51);
+            this.BtnShootingGallery.TabIndex = 2;
+            this.BtnShootingGallery.Text = "Shooting Gallery";
+            this.BtnShootingGallery.UseVisualStyleBackColor = true;
+            this.BtnShootingGallery.Click += new System.EventHandler(this.BtnShootingGallery_Click);
+            // 
+            // numGames
+            // 
+            this.numGames.Location = new System.Drawing.Point(12, 228);
+            this.numGames.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numGames.Name = "numGames";
+            this.numGames.Size = new System.Drawing.Size(75, 22);
+            this.numGames.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Number of Tests";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 324);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Number of People";
+            // 
+            // numPeopleShooting
+            // 
+            this.numPeopleShooting.Location = new System.Drawing.Point(12, 344);
+            this.numPeopleShooting.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numPeopleShooting.Name = "numPeopleShooting";
+            this.numPeopleShooting.Size = new System.Drawing.Size(75, 22);
+            this.numPeopleShooting.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 444);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Driving school peoples test";
+            // 
+            // numDrivingTests
+            // 
+            this.numDrivingTests.Location = new System.Drawing.Point(12, 464);
+            this.numDrivingTests.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numDrivingTests.Name = "numDrivingTests";
+            this.numDrivingTests.Size = new System.Drawing.Size(75, 22);
+            this.numDrivingTests.TabIndex = 8;
+            // 
+            // BtnDrivingSchool
+            // 
+            this.BtnDrivingSchool.Location = new System.Drawing.Point(12, 391);
+            this.BtnDrivingSchool.Name = "BtnDrivingSchool";
+            this.BtnDrivingSchool.Size = new System.Drawing.Size(75, 51);
+            this.BtnDrivingSchool.TabIndex = 7;
+            this.BtnDrivingSchool.Text = "Driving school";
+            this.BtnDrivingSchool.UseVisualStyleBackColor = true;
+            this.BtnDrivingSchool.Click += new System.EventHandler(this.BtnDrivingSchool_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(279, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Casino Problem";
+            // 
+            // numWheelSpins
+            // 
+            this.numWheelSpins.Location = new System.Drawing.Point(279, 231);
+            this.numWheelSpins.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numWheelSpins.Name = "numWheelSpins";
+            this.numWheelSpins.Size = new System.Drawing.Size(75, 22);
+            this.numWheelSpins.TabIndex = 11;
+            // 
+            // BtnRouletteWheel
+            // 
+            this.BtnRouletteWheel.Location = new System.Drawing.Point(282, 157);
+            this.BtnRouletteWheel.Name = "BtnRouletteWheel";
+            this.BtnRouletteWheel.Size = new System.Drawing.Size(75, 51);
+            this.BtnRouletteWheel.TabIndex = 10;
+            this.BtnRouletteWheel.Text = "Spin the Wheel";
+            this.BtnRouletteWheel.UseVisualStyleBackColor = true;
+            this.BtnRouletteWheel.Click += new System.EventHandler(this.BtnRouletteWheel_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(279, 327);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Toy Collectables";
+            // 
+            // numTimesPaidForToy
+            // 
+            this.numTimesPaidForToy.Location = new System.Drawing.Point(279, 347);
+            this.numTimesPaidForToy.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numTimesPaidForToy.Name = "numTimesPaidForToy";
+            this.numTimesPaidForToy.Size = new System.Drawing.Size(75, 22);
+            this.numTimesPaidForToy.TabIndex = 14;
+            // 
+            // BtnToyCollectables
+            // 
+            this.BtnToyCollectables.Location = new System.Drawing.Point(279, 273);
+            this.BtnToyCollectables.Name = "BtnToyCollectables";
+            this.BtnToyCollectables.Size = new System.Drawing.Size(75, 51);
+            this.BtnToyCollectables.TabIndex = 13;
+            this.BtnToyCollectables.Text = "Collect Toys";
+            this.BtnToyCollectables.UseVisualStyleBackColor = true;
+            this.BtnToyCollectables.Click += new System.EventHandler(this.BtnToyCollectables_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 508);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numTimesPaidForToy);
+            this.Controls.Add(this.BtnToyCollectables);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numWheelSpins);
+            this.Controls.Add(this.BtnRouletteWheel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numDrivingTests);
+            this.Controls.Add(this.BtnDrivingSchool);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numPeopleShooting);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numGames);
+            this.Controls.Add(this.BtnShootingGallery);
+            this.Controls.Add(this.BtnPlinko);
+            this.Controls.Add(this.TxtDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numGames)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPeopleShooting)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDrivingTests)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numWheelSpins)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTimesPaidForToy)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtDisplay;
+        private System.Windows.Forms.Button BtnPlinko;
+        private System.Windows.Forms.Button BtnShootingGallery;
+        private System.Windows.Forms.NumericUpDown numGames;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numPeopleShooting;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numDrivingTests;
+        private System.Windows.Forms.Button BtnDrivingSchool;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numWheelSpins;
+        private System.Windows.Forms.Button BtnRouletteWheel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numTimesPaidForToy;
+        private System.Windows.Forms.Button BtnToyCollectables;
+    }
+}
+
---
+++ Probabilistic Simulations/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Probabilistic_Simulations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rand = new Random();
+
+        // ------------------ Question 1 ------------------
+        int[] placementsCounter = new int[8];
+        float currentPlacement;
+
+        void BtnPlinko_Click(object sender, EventArgs e)
+        {
+            //Clears the variables for next click
+            for (int i = 0; i < placementsCounter.Length; i++)
+            {
+                placementsCounter[i] = 0;
+            }
+            currentPlacement = 0;
+
+            SimulatePlinko();
+            DisplayPlinko();
+        }
+        void SimulatePlinko()
+        {
+            int amountTests = (int)numGames.Value;
+
+            // How many times are we going to do the test
+            for (int ball = 0; ball < amountTests; ball++)
+            {
+                currentPlacement = 4f; // set it back to the middle
+
+                // Do a simulation
+                for (int level = 1; level <= placementsCounter.Length; level++)
+                {
+                    int tempRand = rand.Next(2);
+
+                    if (tempRand == 1)
+                        currentPlacement += 0.5f;
+                    else
+                        currentPlacement -= 0.5f;
+                }
+
+                // Prevents index exception
+                if (currentPlacement >= 8 || currentPlacement < 0)
+                {
+                    continue;
+                }
+
+                int temp = (int)currentPlacement;
+                placementsCounter[temp] = placementsCounter[temp] + 1;
+
+            }
+        }
+        void DisplayPlinko()
+        {
+            TxtDisplay.Text = "";
+            TxtDisplay.Text += "Pockets:\tFrequency:" + Environment.NewLine;
+
+            for (int i = 1; i < placementsCounter.Length; i++)
+            {
+                TxtDisplay.Text += i + "\t" + placementsCounter[i] + Environment.NewLine;
+            }
+
+        }
+
+        // ------------------ Question 2 ------------------
+        float playerBuyIn = 25f;
+        int playerShots = 3;
+        float prizeCost = 100f;
+        float plateCost = 0.4f;
+        float ownerTotal;
+        int playerPrizeCounter = 0;
+
+        void BtnShootingGallery_Click(object sender, EventArgs e)
+        {
+            int[] results = SimulateShootingGallery();
+            DisplayShootingGallery(results);
+        }
+        public int[] SimulateShootingGallery()
+        {
+            int amountTurns = (int)numPeopleShooting.Value;
+            int tempRand;
+            int[] shotHitCounter = new int[amountTurns];
+
+            // How many times people are going to try take shots
+            for (int turn = 0; turn < amountTurns; turn++)
+            {
+                ownerTotal += playerBuyIn; // A customer paid
+                playerPrizeCounter = 0; // Set the customers counter for shots to 0
+
+                for (int level = 1; level <= playerShots; level++) // Do a single round of 3 turns for the current shooter
+                {
+                    tempRand = rand.Next(2); // Has a 50/50 chance of either hitting the plate or not
+
+                    if (tempRand == 1)
+                    {
+                        ownerTotal -= plateCost; // subtract the cost of buying a new plate from the owners total amount
+                        playerPrizeCounter++; // Increase the customer counter by one.
+                    }
+                }
+
+                shotHitCounter[turn] = shotHitCounter[turn] + playerPrizeCounter; // Place the total amount of successful shots made for that customer
+
+                if (playerPrizeCounter >= 3) // if someone got all 3 shots
+                {
+                    ownerTotal -= prizeCost; // minus from the owners total the cost of the prize
+                }
+            }
+
+            return shotHitCounter;
+        }
+        void DisplayShootingGallery(int[] _results)
+        {
+            TxtDisplay.Text = "";
+            TxtDisplay.Text += "Number of People:\tNumber of shots:" + Environment.NewLine;
+            float amount = (float)numPeopleShooting.Value;
+
+            for (int i = 1; i < _results.Length; i++)
+            {
+                TxtDisplay.Text += i + "\t\t" + _results[i] + Environment.NewLine;
+            }
+
+            TxtDisplay.Text += "Owner Total: " + ownerTotal.ToString("c");
+            TxtDisplay.Text += Environment.NewLine + "Average amount made per person: " + (ownerTotal / amount).ToString("c");
+        }
+
+        // ------------------ Question 3 ------------------
+        int studentAmountOfDays;
+        int totalAmountOfDays;
+        int probOfFailingClassTest = 15;
+        int probOfFailingDrivingTest = 25;
+
+        private void BtnDrivingSchool_Click(object sender, EventArgs e)
+        {
+            TxtDisplay.Text = "";
+            SimulateDrivingTests();
+        }
+        void SimulateDrivingTests()
+        {
+            int amount = (int)numDrivingTests.Value;
+
+            for (int student = 1; student < amount + 1; student++)
+            {
+                studentAmountOfDays = 0;
+
+                TxtDisplay.Text += "Student " + student + ": "; 
+                ClassTest();
+                YardAndRoadTest();
+
+            }
+
+            TxtDisplay.Text += "Total days: " + totalAmountOfDays;
+            if (amount <= 0)
+                amount = 1;
+
+            MessageBox.Show("The average amount of days it takes to pass both tests is " + totalAmountOfDays / amount);
+        }
+        void ClassTest()
+        {
+            while (true)
+            {
+                studentAmountOfDays += 4;
+                totalAmountOfDays += 4;
+
+                int num = rand.Next(0, 101);
+                if (num > probOfFailingClassTest)
+                    break;
+
+                TxtDisplay.Text += "Failed class test - ";
+            }
+            TxtDisplay.Text += "Passed class test - ";
+        }
+        void YardAndRoadTest()
+        {
+            while (true)
+            {
+                studentAmountOfDays += 10;
+                totalAmountOfDays += 10;
+
+                int num = rand.Next(0, 101);
+                if (num > probOfFailingDrivingTest)
+                    break;
+
+                TxtDisplay.Text += "Failed driving test - ";
+            }
+            TxtDisplay.Text += "Passed driving test. " + Environment.NewLine + "Total days for student: " + studentAmountOfDays + Environment.NewLine;
+        }
+
+        // ------------------ Question 4 ------------------
+        decimal playerRouletteTotal;
+        float rouletteBuyIn = 7.5f;
+        int ballPlacement;
+        int[] rouletteValueFlag = new int[36];
+
+        private void BtnRouletteWheel_Click(object sender, EventArgs e) => SimulateRouletteGame();
+        void SimulateRouletteGame()
+        {
+            int amount = (int)numWheelSpins.Value; // uses a numeric up down control on the form
+            playerRouletteTotal = 0;
+            for (int i = 0; i < rouletteValueFlag.Length; i++) // set the array back to 0 for all values before next round of spins happen
+                rouletteValueFlag[i] = 0;
+
+            for (int games = 0; games < amount; games++)// amount of games
+            {
+                TxtDisplay.Text += "Player Total for game " + (games + 1) + ": " + playerRouletteTotal.ToString("c") + Environment.NewLine;
+                playerRouletteTotal = 0;
+                playerRouletteTotal -= Convert.ToDecimal(rouletteBuyIn); // player buys in for the first time
+
+                while (true)
+                {
+                    // Get a random number
+                    ballPlacement = rand.Next(0, 36);
+
+                    // if landed on a unique number (one that hasn’t been rolled before)
+                    if (rouletteValueFlag[ballPlacement] == 0)
+                    {
+                        // add 1 money to the players total
+                        playerRouletteTotal++;
+                        // mark off number so its not unique
+                        rouletteValueFlag[ballPlacement] = 1;
+                    }
+                    // else you lose
+                    else
+                    {
+                        // stop spinning and reset the valueFlags
+                        for (int i = 0; i < rouletteValueFlag.Length; i++) // set the array back to 0 for all values before next round of spins happen
+                            rouletteValueFlag[i] = 0;
+                        break;
+                    }
+                }
+
+            }
+            // display the playerTotal and average amount made per game
+            TxtDisplay.Text += "Total made: " + playerRouletteTotal.ToString("c") + Environment.NewLine + "Average Made per game: " + Convert.ToDecimal(playerRouletteTotal /amount).ToString("c");
+
+        }
+
+        // ------------------ Question 5 ------------------
+        float moneySpendForThisSim;
+        float playerToyTotal;
+        int toyNumberValue;
+        float toyBuyIn = 25f;
+        int[] toyValueFlag = new int[28];
+
+        private void BtnToyCollectables_Click(object sender, EventArgs e) 
+        {
+            TxtDisplay.Text = "";
+            SimulateToyHelmet();
+        } 
+        void SimulateToyHelmet()
+        {
+            int amount = (int)numTimesPaidForToy.Value;
+            for (int i = 0; i < toyValueFlag.Length; i++) // set the array back to 0 for all values before next round of spins happen
+            {
+                toyValueFlag[i] = 0;
+            }
+            moneySpendForThisSim = 0;
+            playerToyTotal = 0;
+            toyNumberValue = 0;
+
+            for (int timesBoughtToy = 0; timesBoughtToy < amount; timesBoughtToy++)// amount of times they’ve bought the toy
+            {
+                for (int i = 0; i < toyValueFlag.Length; i++) // set the array back to 0 for all values before next round of spins happen
+                {
+                    toyValueFlag[i] = 0;
+                }
+
+                moneySpendForThisSim = 0;
+
+                for (int uniqueToy = 1; uniqueToy <= 28; uniqueToy++)
+                {
+                    do
+                    {
+                        moneySpendForThisSim += toyBuyIn;
+                        toyNumberValue = rand.Next(0, 28);
+                    } while (toyValueFlag[toyNumberValue] == 1);
+
+                    toyValueFlag[toyNumberValue] = 1;
+                }
+
+                TxtDisplay.Text += "Simulation " + (timesBoughtToy + 1) + ": " + moneySpendForThisSim.ToString("c") + Environment.NewLine;
+                playerToyTotal += moneySpendForThisSim;
+
+                }
+
+            float average = playerToyTotal / amount;
+            TxtDisplay.Text += "The average total spent to get all the unique toys is " + average.ToString("c");
+
+            }
+        }
+    }
